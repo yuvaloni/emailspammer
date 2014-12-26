c6b3ca438b818aab3bb52d1380bc305d58b5c85c
yuvaloni <yuvon2@gmail.com>
2014-12-26 17:11:27 +0200

    vghjvgh

---
+++ spammer/WebApplication1/default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        from:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" Width="217px"></asp:TextBox>
+        <br />
+        to:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox2" runat="server" Width="216px"></asp:TextBox>
+        <br />
+        subject:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox3" runat="server" Width="216px"></asp:TextBox>
+        <br />
+        body:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox4" runat="server" Height="220px" TextMode="MultiLine" Width="308px"></asp:TextBox>
+        </br>
+        how many emails?:
+        <asp:TextBox ID="TextBox5" runat="server" Width="318px"></asp:TextBox>
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" OnCheckedChanged="CheckBox1_CheckedChanged" Text="I AM AWARE THAT I AM LEGALLY RESPONSIBLE TO ANY OF THE CONSEQUENCES WHETHER DIRECT AND/OR INDIRECT OF ME USING THIS SOFTWARE" />
+        <br />
+        <div class="g-recaptcha" data-sitekey="6LehuP8SAAAAAD2K8ODX1ogsajg9lLbZEReeB2tZ"></div>
+        <br />
+        <asp:ImageButton ID="ImageButton1" runat="server" Height="127px" ImageUrl="~/BTN.png" OnClick="ImageButton1_Click" Width="473px" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ spammer/WebApplication1/default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            WebRequest W = WebRequest.Create(new Uri("https://www.google.com/recaptcha/api/siteverify?secret=6LehuP8SAAAAAPa7DCMC2J0GMG5g4NMT4sb5etRK&response=" + Request.Form["g-recaptcha-response"]));
+            W.Method = "GET";
+            WebResponse r = W.GetResponse();
+            Stream st = r.GetResponseStream();
+            StreamReader sr = new StreamReader(st);
+            char[] buffer= new char[1024];
+            
+            sr.Read(buffer,0,1024);
+            string resp = new string(buffer);
+            if (TextBox1.Text.ToLower().IndexOf("yuv") != -1 || TextBox2.Text.ToLower().IndexOf("yuv") != -1 || resp.IndexOf("true")==-1 || !CheckBox1.Checked)
+            {
+                if (TextBox1.Text.ToLower().IndexOf("yuv") != -1)
+                {
+                    TextBox1.Text = "Choose another name";
+                }
+                if (TextBox2.Text.ToLower().IndexOf("yuv") != -1)
+                {
+                    TextBox2.Text = "Choose another name";
+                }
+                if(resp.IndexOf("true")==-1)
+                {
+                    Response.Write("I Don't work with robots");
+                }
+                if (!CheckBox1.Checked)
+                {
+                    Response.Write("I Don't work with criminals");
+                }
+            }
+            else
+            {
+                using(SmtpClient s = new SmtpClient())
+                {
+                    s.Host = "smtp.mandrillapp.com";
+                    s.Port = 587;
+                    s.EnableSsl = false;
+                    s.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    s.UseDefaultCredentials = false;
+                    s.Credentials = new NetworkCredential("schnitzel@gmx.com", "jOhSx96ZNHbK0uhSy97ZWQ");
+                    MailMessage m = new MailMessage();
+                    m.From = new MailAddress("schnitzel@gmx.com", TextBox1.Text);
+                    m.To.Add(new MailAddress(TextBox2.Text));
+                    m.Subject=TextBox3.Text;
+                    m.Body = TextBox4.Text;
+                    for(int i =0; i<int.Parse(TextBox5.Text);i++)
+                    {
+                        s.Send(m);
+                    }
+
+
+
+                }
+                Response.Write("SPAMMING COMPLETE!");
+            }
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
